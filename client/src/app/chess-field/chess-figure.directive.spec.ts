ef80dbfab9a53ef72d0b2fed8e1853bd4fb39277
pin4in <sergii.korkh@gmail.com>
2019-01-02 17:37:31 +0200

    remove specs

--- client/src/app/chess-field/chess-figure.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ChessFigureDirective } from './chess-figure.directive';
-
-describe('ChessFigureDirective', () => {
-  it('should create an instance', () => {
-    // const directive = new ChessFigureDirective();
-    // expect(directive).toBeTruthy();
-  });
-});
