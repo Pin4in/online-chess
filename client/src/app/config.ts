0f7bdd5856d2a30148b54e247c6c8f63417eb048
pin4in <sergii.korkh@gmail.com>
2019-02-27 17:39:46 +0200

    provide correct url

--- client/src/app/config.ts
+++ client/src/app/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-  privateApi: 'http://127.0.0.1:3000/api',
-  publicApi: 'http://127.0.0.1:3000'
+  privateApi: `${window.location.origin}/api`,
+  publicApi: window.location.origin
 };
